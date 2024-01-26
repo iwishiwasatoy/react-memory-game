f9475bcf36516f18cf8e4e516c4e4a2420a3dee3
Cristopher <38367188+iwishiwasatoy@users.noreply.github.com>
2024-01-26 15:37:21 -0400

    its fully functional now, but it needs some modifications.

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import '../src/styles/global.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 )
